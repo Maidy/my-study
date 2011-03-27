39ad14524813f0264b31c5ddb3e7e28e0e747187
Steve Yu <suguni@gmail.com>
2011-03-27 22:30:28 +0900

    7장 컨테이너 추가

---
+++ BeginningAndroid/Chapter07/src/yui/android/ScrollDemo.java
@@ -0,0 +1,11 @@
+package yui.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScrollDemo extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.scrolldemo);
+	}
+}
