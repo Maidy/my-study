a297b862e4ae81ba69876cacc74b324d606253e0
Steve Yu <suguni@gmail.com>
2011-04-09 19:30:03 +0900

    clj-oauth 사용해 보기 추가. 기본적인 사용방법 확인했음. 케이스별 사용법 좀 더 확인 필요.

---
+++ clojure/clj-oauth-play/src/clj_oauth_play/core.clj
@@ -0,0 +1,35 @@
+(ns clj-oauth-play.core
+  (:require [oauth.client :as oauth]
+	    [com.twinql.clojure.http :as http]))
+
+(def consumer (oauth/make-consumer CONSUMER-KEY
+				   CONSUMER-SECRET
+				   "https://apis.daum.net/oauth/requestToken"
+				   "https://apis.daum.net/oauth/accessToken"
+				   "https://apis.daum.net/oauth/authorize"
+				   :hmac-sha1))
+
+(def request-token (oauth/request-token consumer "oob"))
+(def approval-uri (oauth/user-approval-uri consumer
+					   request-token))
+
+;; approval-uri에서 verifier 값을 가져오는 방법은?
+(def access-token-response (oauth/access-token consumer
+					       request-token
+					       VERIFIER))
+
+;; parameter가 있을때는 어떻게 해야 하지?
+(def credentials (oauth/credentials consumer
+                                    (:oauth_token access-token-response)
+                                    (:oauth_token_secret access-token-response)
+                                    :POST
+				    "https://apis.daum.net/yozm/v1_0/timeline/home.xml"
+                                    {:status "posting from #clojure with #oauth"}))
+
+(def response (http/post "https://apis.daum.net/yozm/v1_0/timeline/home.xml"
+			 :query (merge credentials 
+				       {:status "posting from #clojure with #oauth"})
+			 :parameters (http/map->params {:use-expect-continue false})
+			 :as :string))
+
+(:content response) ;; contents
---
+++ clojure/clj-oauth-play/test/clj_oauth_play/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-oauth-play.test.core
+  (:use [clj-oauth-play.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
