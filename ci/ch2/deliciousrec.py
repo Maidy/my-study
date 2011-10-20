e92042da9ef6e970d010aeabef47ae1eef021330
Steve Yu <suguni@gmail.com>
2011-10-20 13:22:44 +0900

    programming collective intelligence

--- ci/ch2/deliciousrec.py
+++ ci/ch2/deliciousrec.py
@@ -5,17 +5,38 @@
 # pydelicious.get_popular(tag='programming') #=>
 # pydelicious.dlcs_feed('popular_tagged', format='json', tag='programming')
 # http://feeds.delicious.com/v2/json/popular/programming
+# [{"a": "chuckreynolds",
+#   "dt": "2011-10-11T14:00:00Z",
+#   "u": "http://www.webmasterworld.com/forum92/4332.htm",
+#   "d": "Mod_Rewrite and Regular Expressions Apache Web Server forum at WebmasterWorld",
+#   "t": ["regex", "mod_rewrite", "htaccess", "apache", "regexp", "reference", "regular", "programming", "expressions", "webdev"]},
+#   ...]
 
 # pydelicious.get_urlposts('http://arcturo.github.com/library/coffeescript/index.html') #=>
 # pydelicious.dlcs_feed('url', format='json', urlmd5=md5('http://arcturo.github.com/library/coffeescript/index.html').hexdigest())
 # http://feeds.delicious.com/v2/json/url/664f010c4164c449ba8eab79259f14a7
+# [{"a": "popeax",
+#   "d": "The Little Book on CoffeeScript",
+#   "n": "",
+#   "u": "http://arcturo.github.com/library/coffeescript/index.html",
+#   "t": ["coffeescript", "javascript", "programming", "web browser"],
+#   "dt": "2011-10-11T07:31:05Z"},
+#   ...]
 
 # pydelicious.get_userposts('suguni') #=>
 # pydelicious.dlcs_feed('user', format='json', username='suguni')
 # http://feeds.delicious.com/v2/json/suguni
+# [{"a": "suguni",
+#   "d": "The Little Book on CoffeeScript",
+#   "n": "",
+#   "u": "http://arcturo.github.com/library/coffeescript/index.html",
+#   "t": ["coffeescript", "programming", "tutorial", "development", "books", "ebook"],
+#   "dt": "2011-10-07T09:54:20Z"},
+#   ...]
 
 from pydelicious import get_popular, get_userposts, get_urlposts
 
+# popular post의 user 목록 만들기
 def initializeUserDict(tag, count=5):
     user_dict = {}
 
@@ -27,4 +48,33 @@ def initializeUserDict(tag, count=5):
             user_dict[user] = {}
 
     return user_dict
+
+# user 별 url cross check, 있으면 1, 없으면 0
+# 결국 user/url 표 만들기.
+# recommendation.py에서 만든 critics와 동일하다. 단지 평점이 아닌 1, 0 값만 가진다.
+def fillItems(user_dict):
+
+    all_items = {}
+
+    # Find links posted by all users
+    for user in user_dict:
+        print "finding ... " + user
+        for i in range(3):
+            try:
+                posts = get_userposts(user)
+                break
+            except:
+                print "Failed user " + user + ", retrying"
+                time.sleep(4)
+
+        for post in posts:
+            url = post['u']
+            user_dict[user][url] = 1.0
+            all_items[url] = 1
+
+    for ratings in user_dict.values():
+        for item in all_items:
+            if item not in ratings:
+                ratings[item] = 0.0
+
 
--- ci/ch2/recommendation.py
+++ ci/ch2/recommendation.py
@@ -114,6 +114,8 @@ def getRecommendations(prefs, person, similarity=sim_pearson):
 
         sim = similarity(prefs, person, other)
 
+        # print person + ":" + str(sim)
+        
         if sim <= 0: continue
 
         for item in prefs[other]:
@@ -139,4 +141,69 @@ def transformPrefs(prefs):
             result[item][person] = prefs[person][item]
     return result
 
+
+def calculateSimilarItems(prefs, n=10):
+    # Create a dictionary of items showing which other items they are most similar to.
+    result = {}
+
+    # Invert the preference matrix to be imte-centric
+    itemPrefs = transformPrefs(prefs)
+    c = 0
+    for item in itemPrefs:
+        # Status updates for large datasets
+        c += 1
+        if c % 100 == 0: print "%d / %d" % (c, len(itemPrefs))
+
+        # Find the most similar items to this one
+        scores = topMatches(itemPrefs, item, n=n, similarity=sim_distance)
+        result[item] = scores
+
+    return result
+
+def getRecommendedItems(prefs, itemMatch, user):
+    userRatings = prefs[user]
+    scores = {}
+    totalSim = {}
+
+    # Loop over items rated by this user
+    for (item, rating) in userRatings.items():
+
+        # Loop over items similar to this one
+        for (similarity, item2) in itemMatch[item]:
+
+            # Ignore if this user has already rated this item
+            if item2 in userRatings: continue
+
+            # Weighted sum of rating times similarity
+            scores.setdefault(item2, 0)
+            scores[item2] += similarity * rating
+
+            # Sum of all the similarities
+            totalSim.setdefault(item2, 0)
+            totalSim[item2] += similarity
+
+    # Divide each total score by total weighting to get an average
+    rankings = [(score / totalSim[item], item) for item, score in scores.items()]
+
+    # Return the rankings from highest to lowest
+    rankings.sort()
+    rankings.reverse()
+
+    return rankings
+
+def loadMovieLens(path='ml-100k'):
+    # Get movie titles
+    movies = {}
+    for line in open(path + '/u.item'):
+        (id, title) = line.split('|')[0:2]
+        movies[id] = title
+
+    # Load data
+    prefs = {}
+    for line in open(path + '/u.data'):
+        (user, movieid, rating, ts) = line.split('\t')
+        prefs.setdefault(user, {})
+        prefs[user][movies[movieid]] = float(rating)
+
+    return prefs
 
