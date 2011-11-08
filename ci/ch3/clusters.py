47e4844e2174a6f47a291eca491114da9744fef5
Steve Yu <suguni@gmail.com>
2011-11-08 13:13:06 +0900

    ch3. K-Means Clustering (page 44) 까지

--- ci/ch3/clusters.py
+++ ci/ch3/clusters.py
@@ -15,6 +15,8 @@ def readfile(filename):
         data.append([float(x) for x in p[1:]])
 
     return rownames, colnames, data
+# ex:
+# blognames, words, data = clusters.readfile('blogdata.org.txt')
 
 from math import sqrt
 def pearson(v1, v2):
@@ -136,5 +138,60 @@ def drawnode(draw, clust, x, y, scaling, labels):
         drawnode(draw, clust.right, x+ll, bottom-h2/2, scaling, labels)
     else:
         draw.text((x+5, y-7), labels[clust.id], (0, 0, 0))
+
+    
+def rotatematrix(data):
+    newdata = []
+    for i in range(len(data[0])):
+        newrow = [data[j][i] for j in range(len(data))]
+        newdata.append(newrow)
+    return newdata
+
+import random
+
+def kcluster(rows, distance=pearson, k=4):
+    # Determine the minimum and maximum values for each point
+    ranges = [(min(rows[r][i] for r in range(len(rows))),
+               max(rows[r][i] for r in range(len(rows))))
+              for i in range(len(rows[0]))]
+
+    # Create k randomly placed centroids
+    clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0]
+                 for i in range(len(rows[0]))]
+                for j in range(k)]
+
+    lastmatches = None
+    for t in range(100):
+        print 'Iteration %d' % t
+        bestmatches = [[] for i in range(k)]
+
+        for j in range(len(rows)):
+            row = rows[j]
+            bestmatch = 0
+
+            for c in range(k):
+                d = distance(clusters[c], row)
+                if (d < distance(clusters[bestmatch], row)): bestmatch = c
+
+            bestmatches[bestmatch].append(j)
+
+        if bestmatches == lastmatches: break
+        lastmatches = bestmatches
+
+        # Move the centroids to the average of their members
+        for i in range(k):
+            avgs = [0.0] * len(rows[0])
+            if len(bestmatches[i]) > 0:
+                for rowid in bestmatches[i]:
+                    for m in range(len(rows[rowid])):
+                        avgs[m] += rows[rowid][m]
+                for j in range(len(avgs)):
+                    avgs[j] /= len(bestmatches[i])
+                clusters[i] = avgs
+
+    return bestmatches
+# ex:
+# kclust = clusters.kcluster(data, k=10)
+                
 
     
