39db02cbdad104032576f5220dbb03a1691e1d51
Steve Yu <suguni@gmail.com>
2011-11-26 23:41:38 +0900

    3장 끝. 4장 시작

--- ci/ch3/clusters.py
+++ ci/ch3/clusters.py
@@ -203,3 +203,68 @@ def tanimoto(v1, v2):
         if v1[i] != 0 and v2[i] != 0: shr += 1
 
     return 1 - (float(shr) / (c1 + c2 - shr))
+
+
+# p50 Scale down
+def scaledown(data, distance=pearson, rate=0.01):
+
+    n = len(data)
+    
+    # real distance
+    realdist = [[distance(data[i], data[j])
+                 for j in range(n)]
+                 for i in range(n)]
+
+    # randomly initialize the starting points of the locations in 2D
+    loc = [[random.random(), random.random()] for j in range(n)]
+    fakedist = [[0.0 for i in range(n)] for j in range(n)]
+
+    lasterror = None
+    
+    for m in range(1000):
+
+        # Find projected distance
+        for i in range(n):
+            for j in range(n):
+                fakedist[i][j] = sqrt(sum([pow(loc[i][k] - loc[j][k], 2)
+                                           for k in range(len(loc[i]))]))
+        
+        grad = [[0.0, 0.0] for i in range(n)]
+        totalerror = 0.0
+
+        # Move points
+        for i in range(n):
+            for j in range(n):
+                if i == j: continue
+                    
+                errorterm = (fakedist[j][i] - realdist[j][i]) / realdist[j][i]
+                grad[i][0] += ((loc[i][0] - loc[j][0]) / fakedist[j][i]) * errorterm
+                grad[i][1] += ((loc[i][1] - loc[j][1]) / fakedist[j][i]) * errorterm
+
+                totalerror += abs(errorterm)
+
+        print totalerror
+
+        # if the answer got worse by moving the points, we are done
+        if lasterror and lasterror < totalerror: break
+        lasterror = totalerror
+
+        # Move each of the points by the learning rate times the gradient
+        for i in range(n):
+            loc[i][0] -= rate * grad[i][0]
+            loc[i][1] -= rate * grad[i][1]
+
+
+    return loc
+
+def draw2d(data, labels, jpeg='mds2d.jpg'):
+    img = Image.new('RGB', (2000, 2000), (255, 255, 255))
+    draw = ImageDraw.Draw(img)
+    for i in range(len(data)):
+        x = (data[i][0] + 0.5) * 1000
+        y = (data[i][1] + 0.5) * 1000
+        draw.text((x, y), labels[i], (0, 0, 0))
+    img.save(jpeg, 'JPEG')
+
+
+        
