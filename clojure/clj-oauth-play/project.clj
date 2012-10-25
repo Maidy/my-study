d89c149c18d1eb49a675f3e04dd355aa3dbb3e90
Steve Yu <suguni@gmail.com>
2012-10-25 18:47:05 +0900

    ...

--- clojure/clj-oauth-play/project.clj
+++ clojure/clj-oauth-play/project.clj
@@ -1,6 +1,5 @@
 (defproject clj-oauth-play "1.0.0-SNAPSHOT"
   :description "clj-oauth로 Daum Open API 사용해보기 스파이크"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-		 [org.clojure/clojure-contrib "1.2.0"]
-		 [clj-oauth "1.2.10"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clj-oauth "1.3.1-SNAPSHOT"]]
+  :dev-dependencies [])
--- clojure/clj-oauth-play/src/clj_oauth_play/core.clj
+++ clojure/clj-oauth-play/src/clj_oauth_play/core.clj
@@ -1,35 +1,39 @@
 (ns clj-oauth-play.core
   (:require [oauth.client :as oauth]
-	    [com.twinql.clojure.http :as http]))
+            [clj-http.client :as clent]))
 
-(def consumer (oauth/make-consumer CONSUMER-KEY
-				   CONSUMER-SECRET
-				   "https://apis.daum.net/oauth/requestToken"
-				   "https://apis.daum.net/oauth/accessToken"
-				   "https://apis.daum.net/oauth/authorize"
-				   :hmac-sha1))
+(def consumer (oauth/make-consumer
+               "b09f257a-0d6a-41aa-9924-5a38145713e2" ;;  CONSUMER-KEY
+               "mxb-hGWht3-nv7xPvSr.RAR91J9NhMUgpEIVAtyrVRUlOy87XhdJHA00" ;; CONSUMER-SECRET
+               "https://apis.daum.net/oauth/requestToken"
+               "https://apis.daum.net/oauth/accessToken"
+               "https://apis.daum.net/oauth/authorize"
+               :hmac-sha1))
 
+;; request token
 (def request-token (oauth/request-token consumer "oob"))
+
+;; approval url
 (def approval-uri (oauth/user-approval-uri consumer
-					   request-token))
+                                           (:oauth_token request-token)))
 
-;; approval-uri에서 verifier 값을 가져오는 방법은?
+;; ;; approval-uri에서 verifier 값을 가져오는 방법은?
 (def access-token-response (oauth/access-token consumer
-					       request-token
-					       VERIFIER))
+                                               request-token
+                                               "694794"))
 
-;; parameter가 있을때는 어떻게 해야 하지?
-(def credentials (oauth/credentials consumer
-                                    (:oauth_token access-token-response)
-                                    (:oauth_token_secret access-token-response)
-                                    :POST
-				    "https://apis.daum.net/yozm/v1_0/timeline/home.xml"
-                                    {:status "posting from #clojure with #oauth"}))
+;; ;; parameter가 있을때는 어떻게 해야 하지?
+;; (def credentials (oauth/credentials consumer
+;;                                     (:oauth_token access-token-response)
+;;                                     (:oauth_token_secret access-token-response)
+;;                                     :POST
+;;                                     "https://apis.daum.net/yozm/v1_0/timeline/home.xml"
+;;                                     {:status "posting from #clojure with #oauth"}))
 
-(def response (http/post "https://apis.daum.net/yozm/v1_0/timeline/home.xml"
-			 :query (merge credentials 
-				       {:status "posting from #clojure with #oauth"})
-			 :parameters (http/map->params {:use-expect-continue false})
-			 :as :string))
+;; (def response (http/post "https://apis.daum.net/yozm/v1_0/timeline/home.xml"
+;;                          :query (merge credentials 
+;;                                        {:status "posting from #clojure with #oauth"})
+;;                          :parameters (http/map->params {:use-expect-continue false})
+;;                          :as :string))
 
-(:content response) ;; contents
+;; (:content response) ;; contents
