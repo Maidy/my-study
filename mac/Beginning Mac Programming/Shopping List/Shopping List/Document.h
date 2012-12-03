62a6a2b369b2bd8b855fdccc8c03b9d339e2bfec
Steve Yu <suguni@gmail.com>
2012-12-03 12:39:43 +0900

    mac programming 8장. Shopping List app. 프로젝트 추가

---
+++ mac/Beginning Mac Programming/Shopping List/Shopping List/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  Shopping List
+//
+//  Created by suguni on 12. 12. 2..
+//  Copyright (c) 2012년 YuiWorld. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
