8a2d7610b536dec2fd671f6b8faf04c484114d43
Steve Yu <suguni@gmail.com>
2013-02-24 23:34:08 +0900

    opengl es for android chapter 3

--- android/opengles/AirHockey1/src/com/airhockey/android/AirHockeyRenderer.java
+++ android/opengles/AirHockey1/src/com/airhockey/android/AirHockeyRenderer.java
@@ -1,20 +1,28 @@
 package com.airhockey.android;
 
+import static android.opengl.GLES20.*;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.util.Arrays;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-import static android.opengl.GLES20.*;
+import android.content.Context;
 import android.opengl.GLSurfaceView.Renderer;
+
+import com.airhockey.android.util.LoggerConfig;
+import com.airhockey.android.util.ShaderHelper;
+import com.airhockey.android.util.TextResourceReader;
 
 public class AirHockeyRenderer implements Renderer {
 	
 	private static final int POSITION_COMPONENT_COUNT = 2;
 	private static final int BYTES_PER_FLOAT = 4;
 	
+	/*
 	float[] tableVerticesWithTriangles = {
 			// triangle 1
 			0f, 0f,
@@ -34,10 +42,50 @@ public class AirHockeyRenderer implements Renderer {
 			4.5f, 12f,
 			4.5f, 2f
 	};
+	*/
+	
+	float[] tableVerticesWithTriangles = {
+			// triangle 1
+			-0.5f, -0.5f,
+			 0.5f,  0.5f,
+			-0.5f,  0.5f,
+			
+			// triangle 2
+			-0.5f, -0.5f,
+			 0.5f, -0.5f,
+			 0.5f,  0.5f,
+			
+			// center line
+			-0.5f,  0.0f,
+			 0.5f,  0.0f,
+			
+			// mallets
+			 0.0f, -0.25f,
+			 0.0f,  0.25f
+			 
+			 // center puck
+			 
+			 // table border
+			 
+	};
+
+	private final Context context;
 	
 	private final FloatBuffer vertexData;
 	
-	public AirHockeyRenderer() {
+	private static final String A_POSITION = "a_Position";
+	private static final String[] ATTRIBUTES = { A_POSITION };
+	private static final int A_POSITION_LOCATION =
+			Arrays.asList(ATTRIBUTES).indexOf(A_POSITION);
+	
+	private static final String U_COLOR = "u_Color";
+	private int uColorLocation;
+	
+	private int program;
+	
+	public AirHockeyRenderer(Context context) {
+		this.context = context;
+		
 		vertexData = ByteBuffer
 				.allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
 				.order(ByteOrder.nativeOrder())
@@ -51,6 +99,18 @@ public class AirHockeyRenderer implements Renderer {
 		// OpenGL 2.0에서는 android.opengl.GLES20 클래스의 static method 이용
 		
 		glClear(GL_COLOR_BUFFER_BIT);
+		
+		glUniform4f(uColorLocation, 1.f, 1.f, 1.f, 1.f);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		
+		glUniform4f(uColorLocation, 1.f, 0.f, 0.f, 1.f);
+		glDrawArrays(GL_LINES, 6, 2);
+		
+		glUniform4f(uColorLocation, 0.f, 0.f, 1.f, 1.f);
+		glDrawArrays(GL_POINTS, 8, 1);
+		
+		glUniform4f(uColorLocation, 1.f, 0.f, 0.f, 1.f);
+		glDrawArrays(GL_POINTS, 9, 1);
 	}
 
 	@Override
@@ -60,6 +120,30 @@ public class AirHockeyRenderer implements Renderer {
 
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-		glClearColor(1.f, 0.f, 0.f, 0.f);
+		glClearColor(0.f, 0.f, 0.f, 0.f);
+		
+		String vertexShaderSource = 
+				TextResourceReader.readTextFileFromResource(context,
+						R.raw.simple_vertex_shader);
+		String fragmentShaderSource = 
+				TextResourceReader.readTextFileFromResource(context,
+						R.raw.simple_fragment_shader);
+		
+		int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
+		int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource);
+
+		program = ShaderHelper.linkProgram(vertexShader, fragmentShader, ATTRIBUTES);
+		if (LoggerConfig.ON)
+			ShaderHelper.validateProgram(program);
+		glUseProgram(program);
+		
+		uColorLocation = glGetUniformLocation(program, U_COLOR);
+		
+		vertexData.position(0);
+		glVertexAttribPointer(A_POSITION_LOCATION, POSITION_COMPONENT_COUNT,
+				GL_FLOAT, false, 0, vertexData);
+		
+		glEnableVertexAttribArray(A_POSITION_LOCATION);
+		
 	}
 }
