fdbb3a87e50bbc86978cc52594787f755b06d4d6
Steve Yu <suguni@gmail.com>
2014-02-09 10:51:39 +0900

    .

---
+++ lisp/pcl/test-framework.lisp
@@ -0,0 +1,38 @@
+;; test
+
+(defun test-+ ()
+  (and 
+   (= (+ 1 2) 3)
+   (= (+ 1 2 3) 6)
+   (= (+ -1 -3) -4)))
+
+(defun test-+ ()
+  (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ 1 2) 3) '(= (+ 1 2) 3))
+  (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
+  (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
+
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~] ... ~a~%" result form))
+
+(defun test-+ ()
+  (report-result (= (+ 1 2) 3) '(= (+ 1 2) 3))
+  (report-result (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
+  (report-result (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
+
+(defmacro check (form)
+  `(report-result ,form ',form))
+
+(defun test-+ ()
+  (check (= (+ 1 2) 3))
+  (check (= (+ 1 2 3) 6))
+  (check (= (+ -1 -3) -4)))
+
+(defmacro check (&body forms)
+  `(progn
+	 ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+(defun test-+ ()
+  (check
+	(= (+ 1 2) 3)
+	(= (+ 1 2 3) 6)
+	(= (+ -1 -3) -4)))
